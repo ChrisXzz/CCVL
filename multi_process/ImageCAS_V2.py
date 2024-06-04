7c79185555fd5a9efad672dd680812789655c943
ChrisXzz <xinzezhou97@gmail.com>
2024-06-03 23:35:34 -0400

    Update ImageCAS_V2.py

--- multi_process/ImageCAS_V2.py
+++ multi_process/ImageCAS_V2.py
@@ -7,29 +7,25 @@ from multiprocessing import cpu_count
 from tqdm import tqdm
 
 def process_file(base_path, filename, target_path):
-    """处理单个文件，重命名并移动到目标路径"""
     if filename.endswith('.nii.gz'):
         case_name, case_type = filename.split('.')[:2]
 
-        # 创建案例对应的文件夹
         case_folder_name = f"{os.path.basename(base_path)}_{case_name}"
         case_folder_path = os.path.join(target_path, case_folder_name)
         os.makedirs(case_folder_path, exist_ok=True)
         
         if case_type == 'label':
-            # 创建 segmentations 文件夹
             segmentations_folder_path = os.path.join(case_folder_path, 'segmentations')
             os.makedirs(segmentations_folder_path, exist_ok=True)
-            # 重命名标注文件并移动到 segmentations 文件夹
+
             new_label_filename = f"{case_name}_labels.nii.gz"
             shutil.copy2(os.path.join(base_path, filename), os.path.join(segmentations_folder_path, new_label_filename))
         elif case_type == 'img':
-            # 重命名 ct 影像并移动到案例文件夹
+
             new_ct_filename = 'ct.nii.gz'
             shutil.copy2(os.path.join(base_path, filename), os.path.join(case_folder_path, new_ct_filename))
 
 def reorganize_files(base_path, target_path):
-    """遍历指定路径下的所有文件，并行处理符合命名规则的文件"""
     files = [filename for filename in os.listdir(base_path) if filename.endswith('.nii.gz')]
     
     with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
