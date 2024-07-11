dd114a400b2a8e7e1d3b0b361985487efdd71299
ChrisXzz <xinzezhou97@gmail.com>
2024-07-11 18:59:09 -0400

    Update cross_validation.sh

--- cross_validation/cross_validation.sh
+++ cross_validation/cross_validation.sh
@@ -15,7 +15,7 @@ do
   bash shell_scripts_cv/step1.train.multigpu.sh $ARCH $PRETRAINED_MODEL $BATCH_SIZE $NUM_SAMPLES jhh_train_fold_${i} $i >> logs/segresnet.jhh.fold_${i}.txt
 
   echo "Inference fold $i..."
-  bash shell_scripts_cv/step2.inference.multigpu.sh $ARCH out/$ARCH.jhh.fold_$i/model.pth $ARCH.jhh_fold_${i} $NUM_GPUS
+  bash shell_scripts_cv/step2.inference.multigpu.sh $ARCH out/$ARCH.jhh.fold_$i/model.pth $ARCH.jhh_fold_${i} $NUM_GPUS dataset/dataset_list/jhh_test_fold_${i}.txt
 
   echo "Evaluation fold $i..."
   bash shell_scripts_cv/step3.eval.sh inference/$ARCH.jhh.fold_$i /data/zzhou82/data/JHH_ROI_0.5mm $ARCH.jhh_fold_${i} $i >> logs/segresnet.jhh.fold_${i}.txt
