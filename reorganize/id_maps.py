108959587e378cfbc948718d67fb7b7c8930c93b
ChrisXzz <xinzezhou97@gmail.com>
2024-07-26 11:24:50 -0400

    Update id_maps.py

--- reorganize/id_maps.py
+++ reorganize/id_maps.py
@@ -30,8 +30,8 @@ def rename_folders(source_path, destination_path, prefix="BDMAP_", start_number=
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--source_path', default = '', help='The source directory containing the folders to rename.')
-    parser.add_argument('--destination_path', default = '', help='The destination directory to save the renamed folders.')
+    parser.add_argument('--source_path', required=True, help='The source directory containing the folders to rename.')
+    parser.add_argument('--destination_path', required=True, help='The destination directory to save the renamed folders.')
     parser.add_argument('--prefix', default='BDMAP_', help='The prefix for the new folder names.')
     parser.add_argument('--start_number', type=int, default=15638, help='The starting number for the new folder names.')
     args = parser.parse_args()
